d33d6f3f32532aea323610ebb6f779163f362f81
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-06-27 23:42:30 +0300

    import js files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
+
+import {colors,max,min, randomIntegerFromInterval} from './js/random-fanction'
+
 import './sass/main.scss';
 
+const start = document.querySelector('[data-action="start"]');
+const stop = document.querySelector('[data-action="stop"]');
+let timerColor = null;
 
-const start= document.que
+start.addEventListener('click', changeColorOnlick);
+stop.addEventListener('click', () => {
+  clearInterval(timerColor);
+  start.disabled = false;
+});
+
+
+function changeColorOnlick(evt) {
+    timerColor = setInterval(() => {
+    const color=colors[randomIntegerFromInterval(min, max)]
+        document.body.style.backgroundColor = color;
+    }, 1000);
+    start.disabled = true;
+  
+};
+
+ 
--- src/js/random-fanction.js
+++ src/js/random-fanction.js
@@ -1,3 +1,16 @@
-const randomIntegerFromInterval = (min, max) => {
+export const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+
+export const max = colors.length - 1;
+export const min = 0;
+export const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
